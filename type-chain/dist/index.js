043bc811052442d8d87719299a9e388c21cdd957
Liha Kim <lihakim.kr@gmail.com>
2021-07-16 08:08:02 +0200

    [16/Jul/21] - type-chain done

--- type-chain/dist/index.js
+++ type-chain/dist/index.js
@@ -1,23 +1,62 @@
-class Hooman {
-    constructor(name, age, gender) {
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
-    } // constructor: 클래스가 시작할 때마다 호출 됨
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const CryptoJS = require("crypto-js");
+class Block {
+    constructor(index, hash, previousHash, data, timestamp) {
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = timestamp;
+    }
 }
-const Nenya = new Hooman("nenya", 1, "female");
-const person = {
-    name: "bilbo",
-    age: 1,
-    gender: "female"
+Block.calculateBlockHash = (index, previousHash, timestamp, data) => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+// SHA: Secure Hash Algorithm
+// SHA256: 64 자리 문자열 반환(256비트, 2의 256제곱만큼)
+Block.validateStructure = (aBlock) => typeof aBlock.index === "number" &&
+    typeof aBlock.hash === "string" &&
+    typeof aBlock.previousHash === "string" &&
+    typeof aBlock.timestamp === "number" &&
+    typeof aBlock.data === "string";
+const genesisBlock = new Block(0, "20202020202020", "", "hello", 123456);
+let blockchain = [genesisBlock];
+const getBlockchain = () => blockchain;
+const getLatestBlock = () => blockchain[blockchain.length - 1];
+const getNewTimestamp = () => Math.round(new Date().getTime() / 1000);
+const createNewBlock = (data) => {
+    const previousBlock = getLatestBlock();
+    const newIndex = previousBlock.index + 1;
+    const newTimestamp = getNewTimestamp();
+    const newHash = Block.calculateBlockHash(newIndex, previousBlock.hash, newTimestamp, data);
+    const newBlock = new Block(newIndex, newHash, previousBlock.hash, data, newTimestamp);
+    addBlock(newBlock);
+    return newBlock;
 };
-const sayHi = (person) => {
-    console.log(`hi!, ${person.name}. you are ${person.age} and ${person.gender}`);
+const getHashForBlock = (newBlock) => Block.calculateBlockHash(newBlock.index, newBlock.previousHash, newBlock.timestamp, newBlock.data);
+const isBlockValid = (candidateBlock, previousBlock) => {
+    if (!Block.validateStructure(candidateBlock)) {
+        return false;
+    }
+    else if (previousBlock.index + 1 !== candidateBlock.index) {
+        return false;
+    }
+    else if (previousBlock.hash !== candidateBlock.previousHash) {
+        return false;
+    }
+    else if (getHashForBlock(candidateBlock) !== candidateBlock.hash) {
+        return false;
+    }
+    else {
+        return true;
+    }
 };
-const sayBye = (person) => {
-    // console.log(`hi!, ${person.name}. you are ${person.age} and ${person.gender}`);
-    // error at age: age is private and only assignable within class 'Hooman'
+const addBlock = (candidateBlock) => {
+    if (isBlockValid(candidateBlock, getLatestBlock())) {
+        blockchain.push(candidateBlock);
+    }
 };
-sayHi(person);
-sayBye(Nenya);
+createNewBlock("second Block");
+createNewBlock("third Block");
+createNewBlock("fourth Block");
+console.log(blockchain);
 //# sourceMappingURL=index.js.map
--- type-chain/src/index.ts
+++ type-chain/src/index.ts
@@ -1,36 +1,102 @@
-class Hooman {
-  public name: string;
-  private age: number;
-  public gender: string;
-  constructor(name:string, age:number, gender?:string) {
-    this.name = name;
-    this.age = age;
-    this.gender = gender;
-  } // constructor: 클래스가 시작할 때마다 호출 됨
+import * as CryptoJS from "crypto-js";
+
+class Block {
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
+
+  static calculateBlockHash = (
+    index: number,
+    previousHash: string,
+    timestamp: number,
+    data: string
+  ): string =>
+    CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
+  // SHA: Secure Hash Algorithm
+  // SHA256: 64 자리 문자열 반환(256비트, 2의 256제곱만큼)
+
+  static validateStructure = (aBlock: Block): boolean =>
+    typeof aBlock.index === "number" &&
+    typeof aBlock.hash === "string" &&
+    typeof aBlock.previousHash === "string" &&
+    typeof aBlock.timestamp === "number" &&
+    typeof aBlock.data === "string";
+
+  constructor(
+    index: number,
+    hash: string,
+    previousHash: string,
+    data: string,
+    timestamp: number
+  ) {
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.data = data;
+    this.timestamp = timestamp;
+  }
 }
 
-interface Human { 
-  name: string,
-  age: number,
-  gender: string
-}
+const genesisBlock: Block = new Block(0, "20202020202020", "", "hello", 123456);
 
-
-const Nenya = new Hooman("nenya",1,"female")
-
-const person = {
-  name: "bilbo",
-  age: 1,
-  gender: "female"
-}
-
-const sayHi = (person:Human): void => {
-  console.log(`hi!, ${person.name}. you are ${person.age} and ${person.gender}`);
+let blockchain: Block[] = [genesisBlock];
+const getBlockchain = (): Block[] => blockchain;
+const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
+const getNewTimestamp = (): number => Math.round(new Date().getTime() / 1000);
+const createNewBlock = (data: string): Block => {
+  const previousBlock: Block = getLatestBlock();
+  const newIndex: number = previousBlock.index + 1;
+  const newTimestamp: number = getNewTimestamp();
+  const newHash: string = Block.calculateBlockHash(
+    newIndex,
+    previousBlock.hash,
+    newTimestamp,
+    data
+  );
+  const newBlock: Block = new Block(
+    newIndex,
+    newHash,
+    previousBlock.hash,
+    data,
+    newTimestamp
+  );
+  addBlock(newBlock);
+  return newBlock;
 };
-const sayBye = (person:Hooman): void => {
-  // console.log(`hi!, ${person.name}. you are ${person.age} and ${person.gender}`);
-  // error at age: age is private and only assignable within class 'Hooman'
+
+const getHashForBlock = (newBlock: Block): string =>
+  Block.calculateBlockHash(
+    newBlock.index,
+    newBlock.previousHash,
+    newBlock.timestamp,
+    newBlock.data
+  );
+
+const isBlockValid = (candidateBlock: Block, previousBlock: Block): boolean => {
+  if (!Block.validateStructure(candidateBlock)) {
+    return false;
+  } else if (previousBlock.index + 1 !== candidateBlock.index) {
+    return false;
+  } else if (previousBlock.hash !== candidateBlock.previousHash) {
+    return false;
+  } else if (getHashForBlock(candidateBlock) !== candidateBlock.hash) {
+    return false;
+  } else {
+    return true;
+  }
 };
 
-sayHi(person);
-sayBye(Nenya); 
+const addBlock = (candidateBlock: Block): void => {
+  if (isBlockValid(candidateBlock, getLatestBlock())) {
+    blockchain.push(candidateBlock);
+  }
+};
+
+createNewBlock("second Block");
+createNewBlock("third Block");
+createNewBlock("fourth Block");
+
+console.log(blockchain);
