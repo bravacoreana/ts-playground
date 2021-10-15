9954da873fe4ff42c7af1d57dfff28fe943b05e5
Liha Kim <bravacoreana@gmail.com>
2021-10-15 23:25:03 +0200

    ts-react: add popup and options tsx and css

---
+++ 01-ts-react/src/options/options.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './options.css'
+
+const test = <img src="icon.png" />
+
+const root = document.createElement('div')
+document.body.appendChild(root)
+
+ReactDOM.render(test, root)
